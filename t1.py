089965e3c3b86bb43eeb4c575f8e8cce12013271
Gabriel Garcia Lorencetti <40743828+gabrielgarcia7@users.noreply.github.com>
2021-05-27 17:38:18 -0300

    scenery 2.0

--- t1.py
+++ t1.py
@@ -8,13 +8,14 @@ import math
 # Variáveis globais
 t_x = 0
 t_y = 0
-s_x = 1
-s_y = 1
+s = 1
 angulo = 0
 height = 720
 width = 1280
 numVerticesCirculos = 32
 day_or_night = False
+invertPass = False
+
 
 def criar_matriz():
 	matriz = np.array([ 1.0, 0.0, 0.0, 0.0,
@@ -23,6 +24,7 @@ def criar_matriz():
 						0.0, 0.0, 0.0, 1.0], np.float32)
 
 	return matriz
+
 
 # Retorna a matriz de translação, dado t_x e t_y
 def translacao(t_x, t_y):
@@ -34,6 +36,7 @@ def translacao(t_x, t_y):
 
 	return translacao
 
+
 # Retorna a matriz de escala, dado s_x e s_y
 def escala(s_x, s_y):
 
@@ -43,6 +46,7 @@ def escala(s_x, s_y):
 						0.0, 0.0, 0.0, 1.0], np.float32)
 
 	return escala
+
 
 # Retorna a matriz de rotação, dado o ângulo
 def rotacao(angulo):
@@ -57,18 +61,72 @@ def rotacao(angulo):
 
 	return rotacao
 
+
+def translacao_AR1(t_x, t_y):
+
+	grr = escala = np.array([ 	1/width, 	0.0, 		0.0, (t_x/width)+t_x,
+								0.0, 		1/height, 	0.0, (t_y/height)+t_y,
+								0.0, 		0.0, 		1.0, 0.0,
+								0.0, 		0.0, 		0.0, 1.0], np.float32)
+
+	return grr
+
+
+def translacao_AR2(t_x, t_y):
+
+	grr = escala = np.array([ 	width, 	0.0, 	0.0, (width*t_x)+t_x,
+								0.0, 	height, 0.0, (height*t_y)+t_y,
+								0.0, 	0.0, 	1.0, 0.0,
+								0.0, 	0.0, 	0.0, 1.0], np.float32)
+
+	return grr
+
+
 # Retorna a multiplicação da matriz A pela matriz B
 def multiplica_matriz(matA, matB):
     return ((np.dot(matA.reshape(4,4),matB.reshape(4,4))).reshape(1,16))
 
+
+def rotacao_catavento():	
+	return (multiplica_matriz(multiplica_matriz(translacao_AR1(-0.7, 0.3), rotacao(angulo)), translacao_AR2(+0.7, -0.3)))
+
+
+def escala_balao():
+
+	trans = translacao(-0.1, -0.15)
+	esc = escala(s, s)
+	trans2 = translacao(+0.1, +0.15)	
+
+	return multiplica_matriz(multiplica_matriz(trans, esc), trans2)
+
+
+def translacao_passaro():
+
+	trans = translacao(t_x, t_y)
+
+	if invertPass == True: esc = escala(-1, 1)
+	elif invertPass == False: esc = escala(1, 1)
+
+	return multiplica_matriz(trans, esc)
+
+
 def eventos_teclado(window, key, scancode, action, mods):
 	
-	global t_x, angulo
+	global t_x, angulo, s, invertPass
 
 	if key == 65: angulo += 0.05 #esquerda a
 	if key == 68: angulo -= 0.05 #direita d
-	if key == 263: t_x -= 0.05 #esquerda
-	if key == 262: t_x += 0.05 #direita
+	if key == 263 and t_x > -0.9:
+		t_x -= 0.01 #esquerda
+		invertPass = False
+	if key == 262 and t_x < 0.5:
+		t_x += 0.01 #direita
+		invertPass = True
+	if key == 87 and s <= 1.4:
+		s += 0.005 # escala+
+	if key == 83 and s >= 0.5:
+		s -= 0.005	
+
 
 def eventos_mouse(window, button, action, mods):
 
@@ -81,7 +139,6 @@ def eventos_mouse(window, button, action, mods):
 		elif button == 1:  # direito
 			day_or_night = False
 
-		
 
 def inicializar_janela(height, width):
 	glfw.init()
@@ -94,6 +151,7 @@ def inicializar_janela(height, width):
 	glfw.set_mouse_button_callback(window, eventos_mouse)
 
 	return window
+
 
 # Compila o vertex shader e o fragment shader
 def compilar_shader(program, slot, slot_code, type):
@@ -109,6 +167,7 @@ def compilar_shader(program, slot, slot_code, type):
 	# atribui os shader objects ao programa
 	glAttachShader(program, slot)
 
+
 def criar_circulo(raio, num_vertices, posX, posY):
     
     vertices_circulo = np.zeros(num_vertices, [("position", np.float32, 2)])
@@ -121,6 +180,7 @@ def criar_circulo(raio, num_vertices, posX, posY):
         vertices_circulo[i] = [x,y]
         
     return vertices_circulo
+
 
 def criar_pássaro():
 
@@ -152,10 +212,10 @@ def criar_pássaro():
 
 	return verticesPassaro
 
+
 def criar_catavento():
 	
 	pe = np.zeros(4, [("position", np.float32, 2)])
-
 	pe['position'] = [
 		(-0.715, +0.3),
 		(-0.685, +0.3),
@@ -164,15 +224,13 @@ def criar_catavento():
 	]
 
 	pa1 = np.zeros(3, [("position", np.float32, 2)])
-
 	pa1['position'] = [
 		(-0.7, +0.3),
 		(-0.7, -0.15),
 		(-0.55, +0.15)
 	]
 
-	pa2 = np.zeros(3, [("position", np.float32, 2)])
-	
+	pa2 = np.zeros(3, [("position", np.float32, 2)])	
 	pa2['position'] = [
 		(-0.7, +0.3),
 		(-0.7, +0.75),
@@ -182,15 +240,15 @@ def criar_catavento():
 	pa3 = np.zeros(3, [("position", np.float32, 2)])
 	pa3['position'] = [
 		(-0.70, +0.3),
-		(-0.25, +0.3),
-		(-0.55, +0.45)
+		(-0.40, +0.3),
+		(-0.60, +0.50)
 	]
 
 	pa4 = np.zeros(3, [("position", np.float32, 2)])
 	pa4['position'] = [
 		(-0.70, +0.3),
-		(-1.15, +0.3),
-		(-0.85, +0.15)
+		(-1.00, +0.3),
+		(-0.80, +0.1)
 	]
 
 	centro = criar_circulo(0.08, numVerticesCirculos, -0.7, +0.3)
@@ -198,9 +256,6 @@ def criar_catavento():
 	verticesCatavento = np.concatenate((pe, pa1, pa2, pa3, pa4, centro))
 
 	return verticesCatavento
-
-def rotacao_catavento():
-	return (multiplica_matriz(multiplica_matriz(translacao(-0.7, 0.3), rotacao(angulo)), translacao(+0.7, -0.3)))
 
 
 def criar_casa():
@@ -223,10 +278,11 @@ def criar_casa():
 		(+0.55, -0.60)	# vertice 3 porta
 	]
 
-	janela = criar_circulo(0.1, numVerticesCirculos, 0.5, 0.05)
+	janela = criar_circulo(0.08, numVerticesCirculos, 0.5, -0.10)
 	casa = np.concatenate((estruturaCasa, janela))
 
 	return casa
+
 
 def criar_balao():
 
@@ -240,7 +296,16 @@ def criar_balao():
 		(-0.1, -0.15),	# vertice 2 chão		
 	]
 
-	return np.concatenate((triang_balao, circ_balao))
+	caixa = np.zeros(4, [("position", np.float32, 2)])
+	
+	caixa['position'] = [
+		(-0.15, -0.15),
+		(-0.05, -0.15),
+		(-0.15, -0.30),
+		(-0.05, -0.30)
+	]
+
+	return np.concatenate((triang_balao, circ_balao, caixa))
 
 
 def construir_objetos():
@@ -268,6 +333,7 @@ def construir_objetos():
 	vertices = np.concatenate((verticesA, passaro, sol, catavento, criar_casa(), criar_balao()))
 
 	return vertices
+
 
 def enviar_dados_gpu(vertices):
 	# requisitando um slot de buffer da GPU
@@ -311,8 +377,7 @@ def mostrar_janela(window, program, vertices):
 		glDrawArrays(GL_TRIANGLE_STRIP, 4, 4)
 
 		### Desenhando o pássaro ###
-		mat_translation = multiplica_matriz(translacao(t_x, t_y), escala(s_x, s_y))
-		glUniformMatrix4fv(loc, 1, GL_TRUE, mat_translation)
+		glUniformMatrix4fv(loc, 1, GL_TRUE, translacao_passaro())
 		glUniform4f(loc_color, 1.0, 1.0, 1.0, 1.0)
 		glDrawArrays(GL_TRIANGLES, 8, 3)
 
@@ -363,7 +428,7 @@ def mostrar_janela(window, program, vertices):
 		glUniform4f(loc_color, 1.0, 0.98, 0.5, 1.0)
 		glDrawArrays(GL_TRIANGLE_STRIP, 25+numVerticesCirculos*2+16, 4)
 
-		#glUniform4f(loc_color, 0.8, 0.8, 0.8, 1.0)
+		glUniform4f(loc_color, 0.9, 0.1, 0.1, 1.0)
 		glDrawArrays(GL_TRIANGLES, 25+numVerticesCirculos*2+20, 3)
 
 		glUniform4f(loc_color, 160/255, 82/255, 45/255, 1.0)
@@ -374,16 +439,22 @@ def mostrar_janela(window, program, vertices):
 
 
 		######### --- Balão --- #########
+		glUniformMatrix4fv(loc, 1, GL_TRUE, escala_balao())
+
 		glUniform4f(loc_color, 148/255, 0.0, 211/255, 1.0)
 		glDrawArrays(GL_TRIANGLES, 25+numVerticesCirculos*3+27, 3)
 
 		#glUniform4f(loc_color, 0.7, 0.2, 0.5, 1.0)
 		glDrawArrays(GL_TRIANGLE_FAN, 25+numVerticesCirculos*3+30, numVerticesCirculos)
+
+		glUniform4f(loc_color, 160/255, 82/255, 45/255, 1.0)
+		glDrawArrays(GL_TRIANGLE_STRIP, 25+numVerticesCirculos*4+30, 4)
 		
 
 		glfw.swap_buffers(window)
 
 	glfw.terminate()
+
 
 def main():
 
